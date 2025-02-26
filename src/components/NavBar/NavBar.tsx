9bb0c7c495a9949f115d5816c1cf8775134307ed
tac3563 <tcox_bu@outlook.com>
2025-02-26 21:58:24 +0000

    [Update] set up colour and spacing variables from style guide and set up sass with nav bar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,20 @@
+import './NavBar.scss'
+
+export default function NavBar() {
+    return (
+        <>
+        <header>
+            <nav>
+                {/*<img className='profile-photo' src="#" alt="#"/>*/}
+                <ul>
+                    <li>Home</li>
+                    <li>Blog</li>
+                    <li>About</li>
+                    <li>Newsletter</li>
+                    <li>☀️</li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
