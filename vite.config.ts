db2fc5358296a3202457b25604dc6820898719e9
tac3563 <tcox_bu@outlook.com>
2025-02-27 23:23:15 +0000

    [Update] added global base typography and colour styling

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "/src/styles/typography" as *;`,
+        additionalData: '@use "/src/styles/global" as *;',
       },
     },
   },
