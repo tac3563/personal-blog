ab9e829248a58f0b90c50ee86a5d6a275afefbba
tac3563 <tcox_bu@outlook.com>
2025-03-02 22:20:23 +0000

    [Update] removed unused stylesheets, updated global variable, and styled the navbar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,14 +5,30 @@ export default function NavBar() {
     <>
       <header>
         <nav>
-          {/*<img className='profile-photo' src="#" alt="#"/>*/}
-          <ul>
-            <li>Home</li>
-            <li>Blog</li>
-            <li>About</li>
-            <li>Newsletter</li>
-            <li>☀️</li>
-          </ul>
+          <div className="nav-wrapper">
+            <img
+              className="profile-photo"
+              src="/src/assets/images/image-avatar.jpg"
+              alt="#"
+            />
+            <ul>
+              <li>
+                <a className="active" href="#">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/blog">Blog</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/newsletter">Newsletter</a>
+              </li>
+              <li>☀️</li>
+            </ul>
+          </div>
         </nav>
       </header>
     </>
